7931ed509a2b42489cf5f4d28329eb4d7ee3a09a
zelin <330302700@qq.com>
2020-11-14 17:06:22 +0800

    初始化项目

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <Home></Home>
+  </div>
+</template>
+
+<script>
+import Home from './pages/home'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app,html,body {
+  width: 100%;
+  height: 100%;
+  margin: 0;
+  padding: 0;
+}
+</style>
